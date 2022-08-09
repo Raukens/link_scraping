fd9dbb39033dfe5b2a4b6265c03247b84c93afe7
raukens <rauan.kumash@gmail.com>
2022-08-09 16:54:53 +0600

    оставлены только входящие данные

--- interface.py
+++ interface.py
@@ -1,28 +1,9 @@
-import requests
-
 import sys
 
 
-def get_user_data():
+site = sys.argv[1]
 
-    while True:
-        site = sys.argv[1]
-        if '.' in site:
-            if 'https://' in site:
-                site = str(site)
-            else:
-                site = str(f"https://{site}")
-                response = requests.get(site)
-                if response.status_code == 200:
-                    print('Сайт проходит процедуру парсинга')
-        else:
-            site = input('Вы ввели некорректное название сайта. Пожалуйста, попробуйте снова ')
-        depth = sys.argv[2]
-        if depth.isdigit() is True:
-            depth = int(depth)
-        else:
-            depth = input('Вы ввели не целое число. Пожалуйста, попробуйте снова ')
+depth = sys.argv[2]
 
-        return [site, depth]
 
 
